7aa6a37b94a19bfccc020436e3f79f79ec8f2425
João Píxeles <joaopixeles@users.noreply.github.com>
2020-07-03 20:42:18 -0500

    [R0-Day|00|99] #100DaysOfCode #javascript
    Estoy programando desde el celular 🤣 voy a probar este modelo para ver
    si puedo compartir otro tipo de contenido, estas son pruebas para el
    futuro.

---
+++ R0D0.js
@@ -0,0 +1,8 @@
+console.log("hola mundo")
+
+// estoy programando desde el celular jajaja voy a probar este modelo para ver si puedo compartir otro tipo de contenido, estas son pruebas para el futuro.
+
+
+const json = {"hola": "mundo"}
+
+console.log(json)
